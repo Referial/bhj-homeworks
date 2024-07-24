a884571435c20cb1ec9688116e603a3b24195558
Nikita M <MayorovNikita7@yandex.ru>
2024-07-24 22:39:21 +0500

    first commit cookie-clicker

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,16 +2,18 @@
 
 const cookieImage = document.getElementById("cookie");
 let cookieClick = document.getElementById("clicker__counter");
+let clicks = 0;
 let cookieSpeedClick = document.getElementById("clicker__speed__counter");
 let todayTime = Date.now();
 let previousClicks = 0;
 
 cookieImage.onclick = function () {
-    cookieImage.classList.contains('clicker__cookie') ? cookieImage.className = 'new__clicker__cookie' : cookieImage.className = 'clicker__cookie';
+    cookieClick.textContent = ++clicks;
+    if (clicks % 2 == 0) {
+        --cookie.width;
+    } else {
+        ++cookie.width;
+    }
 
-    let currentClicks = parseInt(cookieClick.textContent);
-    cookieClick.textContent = currentClicks + 1;
-
-    cookieSpeedClick.textContent = (((Date.now() - todayTime) / 1000) / (currentClicks - previousClicks)).toFixed(2);
-    previousClicks = currentClicks;
+    cookieSpeedClick.textContent = (((Date.now() - todayTime) / 1000) / (clicks - (clicks - 1))).toFixed(2);
 };
